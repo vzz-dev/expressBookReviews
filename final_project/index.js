b80ecbf2484bf7976f3de333fefd9c4f95572a56
vzz-dev <workxoff@gmail.com>
2026-05-05 15:34:14 -0400

    primer commit, completando middleware customer aut

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,35 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
- 
-const PORT =5000;
+app.use("/customer/auth/*", function auth(req, res, next) {
+
+    const authData = req.session.authorization;
+    if (!authData) {
+        return res.status(403).json({ message: "User not logged in" });
+    }
+    const token = authData.accessToken;
+
+    if (!token) {
+        return res.status(403).json({ message: "Token not found" });
+    }
+
+    jwt.verify(token, "access", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid token" });
+        }
+
+        req.user = user;
+        next();
+    });
+})
+
+
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
