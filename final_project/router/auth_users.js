30c3e3ba20dd14585f20bd4af522a022bf14f281
vzz-dev <workxoff@gmail.com>
2026-05-08 17:27:27 -0400

    tarea 7 completada

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,6 +4,7 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+const SECRET = "access";
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
